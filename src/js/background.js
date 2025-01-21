28a58f7cea8b7bf51f107734b865382ed41db4af
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2025-01-21 10:06:31 +0000

    Update background.js

--- src/js/background.js
+++ src/js/background.js
@@ -5,28 +5,29 @@ let lastRightClickText = '';
 chrome.runtime.onMessage.addListener((message) => {
     if (message.type === 'rightClickText') {
         lastRightClickText = message.text;
+        chrome.contextMenus.update("addToNotes", {
+            title: `Add "${lastRightClickText || chrome.tabs.TAB?.title || 'page'}" to Snippy Fox`
+        });
     }
 });
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ notes: [] });
   chrome.contextMenus.create({
       id: "addToNotes",
-      title: "Add %s to Snippy Fox",
-      contexts: ["selection", "link"],
+      title: "Add %s to Snippy Fox",  // Will show selected text
+      contexts: ["selection", "link", "page"],
   });
 });
 
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
   if (info.menuItemId === "addToNotes" && tab?.id) {
-      const textToSave = info.selectionText || info.linkText || lastRightClickText || tab.title;
-      if (textToSave) {
-          await addNote(textToSave, tab.url);
-          
-          const { closeAfterSave = false } = await chrome.storage.sync.get(['closeAfterSave']);
-          if (closeAfterSave) {
-              chrome.tabs.remove(tab.id);
-          }
+      const textToSave = info.selectionText || `${tab.title} - ${lastRightClickText}` || tab.title;
+      await addNote(textToSave, tab.url);
+      
+      if (await chrome.storage.sync.get('closeAfterSave').then(({closeAfterSave}) => closeAfterSave)) {
+          chrome.tabs.remove(tab.id);
       }
-      lastRightClickText = ''; // Clear after use
+      lastRightClickText = '';
   }
 });
